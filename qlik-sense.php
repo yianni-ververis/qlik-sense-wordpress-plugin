d2d0d5b0bc62394bce53779fc6c73559eb8947ce
Yianni Ververis <yiannisver@gmail.com>
2017-11-06 12:48:53 -0500

    clean up code

--- qlik-sense.php
+++ qlik-sense.php
@@ -2,7 +2,12 @@
 	/*
 	Plugin Name: Qlik Sense
 	Plugin URI: https://github.com/yianni-ververis/qlik-sense-wordpress-plugin
-	Description: a plugin to connect to Qlik Sense server and get the objects
+	Description: 
+		A plugin to connect to Qlik Sense server and get the objects. 
+		- Unzip the plugin into your plugins directory
+		- Activate from the admin panel
+		- Go to "Qlik Sense" settings and add the host, virtual proxy and the app id
+		- then add the shortcode into your posts "[sense-object qvid="ZwjJQq" height="400" nointeraction="true"]"
 	Version: 0.1
 	Author: yianni.ververis@qlik.com
 	License: MIT
@@ -12,6 +17,14 @@
     define( 'QLIK_SENSE_PLUGIN_MINIMUM_WP_VERSION', '4.0' );
     define( 'QLIK_SENSE_PLUGIN_PLUGIN_DIR', plugin_dir_url( __FILE__ ) );
 
+	// Create the options to be saved in the Database
+	add_action( 'admin_init', 'qlik_sense_plugin_settings' );	
+	function qlik_sense_plugin_settings() {
+		register_setting( 'qlik_sense-plugin-settings-group', 'qs_host' );
+		register_setting( 'qlik_sense-plugin-settings-group', 'qs_prefix' );
+		register_setting( 'qlik_sense-plugin-settings-group', 'qs_id' );
+	}
+
 	// Get the CSS and JS from Sense
     add_action( 'wp_enqueue_scripts', 'my_enqueued_assets' );
     function my_enqueued_assets() {
@@ -19,7 +32,6 @@
 		wp_enqueue_script( 'qlik-sense', 'https://'.esc_attr( get_option('qs_host') ).'/resources/assets/external/requirejs/require.js', array(), '1.0.0' );
 
 		// Register the script
-		// wp_register_script( 'qlik-sense-js', QLIK_SENSE_PLUGIN_PLUGIN_DIR . 'index.js', array('jquery'), '1.0.0' );
 		wp_register_script( 'qlik-sense-js', QLIK_SENSE_PLUGIN_PLUGIN_DIR . 'index.js', array('jquery'), '1.0.0' );
 
 		// Localize the script with new data
@@ -33,28 +45,20 @@
 		// Enqueued script with localized data.
 		wp_enqueue_script( 'qlik-sense-js' );
 	}
-
-	function my_plugin_menu() {
-		add_menu_page('My Plugin Settings', 'Qlik Sense', 'administrator', 'my-plugin-settings', 'my_plugin_settings_page', 'dashicons-admin-generic');
+	
+	add_action('admin_menu', 'qlik_sense_plugin_menu');
+	function qlik_sense_plugin_menu() {
+		add_menu_page('Qlik Sense Plugin Settings', 'Qlik Sense', 'administrator', 'qlik_sense_plugin_settings', 'qlik_sense_plugin_settings_page', 'dashicons-admin-generic');
 	}
 	
-	// Create the options to be saved in the Database
-	add_action( 'admin_init', 'my_plugin_settings' );	
-	function my_plugin_settings() {
-		register_setting( 'my-plugin-settings-group', 'qs_host' );
-		register_setting( 'my-plugin-settings-group', 'qs_prefix' );
-		register_setting( 'my-plugin-settings-group', 'qs_id' );
-	}
-
 	// Create the Admin Setting Page
-	add_action('admin_menu', 'my_plugin_menu');
-	function my_plugin_settings_page() {
+	function qlik_sense_plugin_settings_page() {
 ?>
 		<div class="wrap">
 			<h2>Qlik Sense Plugin Settings</h2>
 			<form method="post" action="options.php">
-				<?php settings_fields( 'my-plugin-settings-group' ); ?>
-				<?php do_settings_sections( 'my-plugin-settings-group' ); ?>
+				<?php settings_fields( 'qlik_sense-plugin-settings-group' ); ?>
+				<?php do_settings_sections( 'qlik_sense-plugin-settings-group' ); ?>
 				<table class="form-table">
 					<tr valign="top">
 						<th scope="row">Host</th>
